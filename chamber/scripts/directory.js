7079961d2a792d39574eb445c573cf7354f4daea
thesaisema <sem19001@byui.edu>
2023-07-01 20:51:17 -0600

    directory changes

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -4,37 +4,39 @@ async function getMembersData() {
     const response = await fetch(url);
     const data = await response.json();
     console.table(data.members);  // note that we reference the prophet array of the data object given the structure of the json file
-    displayProphets(data.members)
+    displayBusiness(data.members)
   }
 
-  const displayProphets = (prophets) => {
-    const cards = document.querySelector('directory-main-cards'); // select the output container element
+  const displayBusiness = (businesses) => {
+    const cards = document.querySelector('#directory-data'); // select the output container element
   
-    prophets.forEach((prophet) => {
+    businesses.forEach((business) => {
       // Create elements to add to the div.cards element
       let card = document.createElement('section');
-      card.innerHTML = `<h2>${member.name}</h2>
-      <img src="${member.imageURL}" alt="member image">
-      <p>${member.street}</p>
-      <p>${member.city}</p>
+      card.classList.add("business-card")
+      card.innerHTML = `<h2>${business.name}</h2>
+      <img src="${business.imageURL}" alt="member image">
+      <p>${business.street}</p>
+      <p>${business.city}</p>
       <p><a href="#">Website</a></p>`;
-  
-      // Build the h2 content out to show the prophet's full name - finish the template string
-      h2.textContent = `${member.name} ${member.lastname}} ____________`;
-  
-      // Build the image portrait by setting all the relevant attribute
-      portrait.setAttribute('src', member.imageurl);
-      portrait.setAttribute('alt', `Portrait of ${member.name} ${member.lastname} ______________`);
-      portrait.setAttribute('loading', 'lazy');
-      portrait.setAttribute('width', '340');
-      portrait.setAttribute('height', '440');
-  
-      // Append the section(card) with the created elements
-      card.appendChild(h2);
-      card.appendChild(portrait);
   
       cards.appendChild(card);
     }) // end of forEach loop
   } // end of function expression
   
   getMembersData();
+
+  function displayGrid()
+  {
+    document.querySelector('#directory-data').classList.remove('list-directory')
+    document.querySelector('#directory-data').classList.add('cards-directory')
+  }
+
+  function displayList()
+  {
+    document.querySelector('#directory-data').classList.remove('cards-directory')
+    document.querySelector('#directory-data').classList.add('list-directory')
+  }
+
+  document.querySelector('#directory-grid').addEventListener('click', displayGrid)
+  document.querySelector('#directory-list').addEventListener('click', displayList)
